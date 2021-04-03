1eb6157ebddada8fe51d797901a5636683b15a5a
Xinyue <71954812+wxy929@users.noreply.github.com>
2021-04-02 20:45:02 -0500

    Add files via upload

---
+++ MDS2021/data-raw/LN_throat_info.R
@@ -0,0 +1,15 @@
+## code to prepare `Logistic_normal_throat_logitnorminfo` dataset goes here
+
+# rm(list = ls())
+
+# retrieve paths to datafiles
+filename = system.file(
+  "extdata",
+  "LN_throat_info.RData",
+  package = "MDS2021"
+)
+
+# read the file
+load(file = filename)
+
+usethis::use_data(LN_throat_info, overwrite = TRUE)
---
+++ MDS2021/data-raw/Smits.R
@@ -0,0 +1,15 @@
+## code to prepare `Smits` dataset goes here
+
+# rm(list = ls())
+
+# retrieve paths to datafiles
+filename = system.file(
+  "extdata",
+  "Smits.RData",
+  package = "MDS2021"
+)
+
+# read the file
+load(file = filename)
+
+usethis::use_data(Smits, overwrite = TRUE)
