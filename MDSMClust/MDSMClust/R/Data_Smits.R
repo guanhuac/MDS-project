55e8af1547626e98dd2026b98d80fadd7fe3e96f
Xinyue Wang <71954812+wxy929@users.noreply.github.com>
2025-06-11 02:17:22 -0400

    Update the whole package

---
+++ MDSMClust/MDSMClust/R/Data_Smits.R
@@ -0,0 +1,17 @@
+#' Smits Data
+#'
+#' Smits data is a data set from the R package "MicrobiomeCluster" including OTU table,
+#' tree and sample seasonal information. The samples are from the early wet season and the late dry season.
+#'
+#' @docType data
+#' @name Smits
+#' @usage data(Smits)
+#' @format A large list with 3 components which are tree, sampleinfo and otutable.
+#' @keywords datasets
+#' @references Smits, S. A., Leach, J., Sonnenburg, E. D., Gonzalez, C. G., Lichtman, J. S., Reid, G., Knight, R., Manjurano, A., Changalucha, J., Elias, J. E., Dominguez-Bello, M. G., and Sonnenburg, J. L. (2017). Seasonal cycling in the gut microbiome of the hadza hunter- gatherers of Tanzania. Science, 357(6353):802-806.
+#'
+#' @examples
+#' data(Smits)
+#' table(Smits$sampleinfo)
+#'
+NULL
